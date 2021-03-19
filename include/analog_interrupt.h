a1a04c0b58046f349bcd27f393eaf9842439248d
huloves <17791627852@163.com>
2021-03-19 22:09:41 +0800

    back

--- include/analog_interrupt.h
+++ include/analog_interrupt.h
@@ -5,12 +5,6 @@
 
 // extern uint64_t ticks;
 
-enum interrupt_status
-{
-    INTERRUPT_OFF,
-    INTERRUPT_ON
-};
-
 /**
  * interrupt_enable - 中断使能
  * **/
@@ -19,12 +13,6 @@ void interrupt_enable();
 /**
  * interrupt_disable - shield interrupt
  * **/
-sigset_t interrupt_disable();
-
-/**
- * interrupt_set - 设阻塞信号集为old_set
- * @old: 要设置的信号集
- * **/
-void interrupt_set(sigset_t old_set);
+void interrupt_disable();
 
 #endif
